8c1d6d4ccddb457b6cf8af0472c305e2613a65b4
R K <blah@eample.com>
2019-04-10 14:09:59 +0200

    Updates

---
+++ precache-manifest.e2830858c89b87c0cfeafa12900dcbd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f00a67b45547d0af9c5",
+    "url": "/react-myapps-auth/static/js/runtime~main.7f00a67b.js"
+  },
+  {
+    "revision": "54c432410b9cb08d2a7d",
+    "url": "/react-myapps-auth/static/js/main.54c43241.chunk.js"
+  },
+  {
+    "revision": "98c278dea83fb65ef785",
+    "url": "/react-myapps-auth/static/js/2.98c278de.chunk.js"
+  },
+  {
+    "revision": "54c432410b9cb08d2a7d",
+    "url": "/react-myapps-auth/static/css/main.859a48cd.chunk.css"
+  },
+  {
+    "revision": "98c278dea83fb65ef785",
+    "url": "/react-myapps-auth/static/css/2.5de363f7.chunk.css"
+  },
+  {
+    "revision": "0b48a5aeb6b9dd7bc9a4227d5d99dce2",
+    "url": "/react-myapps-auth/index.html"
+  }
+];
